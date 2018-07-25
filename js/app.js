14e08555bd3e164ec53be2a52d821f52c82683e0
Cuza <baopham92@yahoo.com>
2018-07-25 17:16:17 -0400

    Initial commit.
    Functionalities currently working: Reset, clickable cards, css animation upon match/nonmatch.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,130 @@
-/*
- * Create a list that holds all of your cards
- */
+document.addEventListener('DOMContentLoaded', function () {
 
+    // GLOBAL DATA
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+    // Deck of cards. (Current static state of elements)
+    const deck = document.querySelector('.deck');
+    console.log(deck); // Confirmination console message.
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    // Deck of cards upon interaction. (Dynamic elements storage.)
+    let clickedCards = [];
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+    // Restart button.
+    const restartButton = document.querySelector('.restart');
+    console.log(restartButton); // Confirmination console message.
+
+    // Cards.
+    const cards = document.querySelectorAll('.card');
+    console.log(cards); //Confirmination console message.
+
+    // FUNCTIONS
+
+    // On load reset board.
+    window.onload = reset();
+
+    // Animate elements. (cards)
+    deck.addEventListener('click', function (e) {
+
+        // Event delegation. (click listener)
+        const eClick = e.target;
+        // console.log(eClick); Confirmination message to console. 
+
+        // Condition for click functions.
+        if (eClick.classList.contains('card') && clickedCards.length < 2 && !clickedCards.includes(eClick)) {
+            pickCard(eClick);
+            depositCard(eClick);
+
+            // Condition for checking element selection. (cards matching)
+            clickedCards.length === 2 ? ifMatch() : console.log(`No match.`);
+        }
+
+        // Condition for matching elements. (cards)
+        function ifMatch() {
+            if (clickedCards[0].innerHTML === clickedCards[1].innerHTML) {
+                clickedCards[0].classList.toggle('match');
+                clickedCards[1].classList.toggle('match');
+
+                clickedCards = []; // Clear array so the next match can sequence.
+
+                console.log(`Matching cards!`); // Confirmination message to console for a match.
+            } else if (clickedCards[0].innerHTML !== clickedCards[1].innerHTML) {
+
+                // Timer for element clearing. (cards)
+                setTimeout( () => {
+
+                    // Toggle the index of the array for elements.
+                    pickCard(clickedCards[0]);
+                    pickCard(clickedCards[1]);
+
+                    // Clear array so the next match can sequence. 
+                    clickedCards = [];
+
+                } , 1500);
+
+                // Logging false match to message to console.
+                console.log(`No match found.`)
+
+            };
+        }
+
+        // Element toggle class function.
+        function pickCard(eClick) {
+
+            // Toggle class add/remove to elements. (cards)
+            eClick.classList.toggle('open');
+            eClick.classList.toggle('show');
+        }
+
+        // Deposit clicked cards
+        function depositCard(eClick) {
+
+            // Adding target delegation to clickedCards array.
+            clickedCards.push(eClick);
+
+            console.log(clickedCards); // Confirmination message.
+        }
+    });
+
+    // Restart board state.
+    restartButton.addEventListener('click', function (e) {
+
+        // Iteration for removing classes to restart.
+        reset();
+
+        // Confirmination console message.
+        console.log(`Restart button clicked!`);
+    })
+
+    function reset() {
+
+        // Iteration for removing classes to restart.
+        for (const card of cards) {
+            card.classList.remove('match', 'open', 'show');
+        }
     }
 
-    return array;
-}
+    /*
+     * Display the cards on the page
+     *   - shuffle the list of cards using the provided "shuffle" method below
+     *   - loop through each card and create its HTML
+     *   - add each card's HTML to the page
+     */
 
+    // Shuffle function from http://stackoverflow.com/a/2450976
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
 
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+})
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
